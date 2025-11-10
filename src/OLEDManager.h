67309386019fe992de93048926db97e5be84f37e
xsismadn3ss <2477carlos.artiga@gmail.com>
2025-11-10 01:02:24 -0600

    🏗️  definir header para manager de pantalla LED

---
+++ src/OLEDManager.h
@@ -0,0 +1,28 @@
+#ifndef OLEDMANAGER_H
+#define OLEDMANAGER_H
+
+// Incluimos las librerías necesarias para la clase
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+/**
+ * @class OLEDManager
+ * @brief Gestiona toda la lógica de visualización en la pantalla OLED.
+ */
+class OLEDManager {
+public:
+    // Constructor: Requiere una referencia a la instancia de Adafruit_SSD1306
+    OLEDManager(Adafruit_SSD1306 &display);
+
+    // Métodos públicos (API para usar desde el main.cpp)
+    void begin();
+    void showWelcomeMessage(String rc522_status);
+    void showAmountToPay(float amount);
+    void showCardUID(String uid);
+
+private:
+    // Referencia privada a la instancia de la pantalla para poder manipularla.
+    Adafruit_SSD1306 &_display; 
+};
+
+#endif
